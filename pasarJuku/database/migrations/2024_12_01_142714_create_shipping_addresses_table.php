1c4d21f1321f2b8318cf3c13b8e2afc75b19b8ce
denywa <dwa1503@gmail.com>
2024-12-04 00:21:08 +0800

    payment migration

--- pasarJuku/database/migrations/2024_12_01_142714_create_shipping_addresses_table.php
+++ pasarJuku/database/migrations/2024_12_01_142714_create_shipping_addresses_table.php
@@ -1,27 +1,36 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class  extends Migration
 {
-    /**
+        /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('shipping_addresses', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('shippingAddress');
+        Schema::create('shippingAddress', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id('shippingAddressID');
+            $table->string('address', 100);
+            $table->string('city', 100);
+            $table->integer('pos_code');
+            $table->string('recipient_name', 100);
+            $table->string('phone', 45);
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('shipping_addresses');
+        Schema::dropIfExists('shippingAddress');
     }
 };
---
+++ pasarJuku/database/migrations/2024_12_03_161906_create_payments_table.php
@@ -0,0 +1,36 @@
+
+<?php
+        /**
+     *namespace Database\Migrations;
+     */
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class  extends Migration
+{
+        /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('payment');
+        Schema::create('payment', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('paymentID');
+            $table->enum('payment_type', ['BCA', 'gopay', 'DANA', 'OVO']);
+        });
+ Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+};
