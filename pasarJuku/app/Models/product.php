c5f6ffbef4da13982902f1ca1fc0093cb71ced87
denywa <dwa1503@gmail.com>
2024-12-05 23:58:36 +0800

    update

--- pasarJuku/app/Models/product.php
+++ pasarJuku/app/Models/product.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class product extends Model 
 {
-    //
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $primaryKey = 'productID';
+    public $incrementing = true;
 }
