de2f50ae3d39c467ed45f762670a8628ef00b8a6
feliciawijaya2006 <148602986+feliciawijaya2006@users.noreply.github.com>
2024-12-18 16:05:23 +0800

    user

--- pasarJuku/routes/api.php
+++ pasarJuku/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\AuthController;
 
 // get user
@@ -18,3 +19,10 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 //reset-password
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
+//user
+Route::get('/users', [UserController::class, 'index']); // Retrieve all users
+Route::get('/users/{id}', [UserController::class, 'show']); // Retrieve a single user by ID
+Route::post('/users', [UserController::class, 'store']); // Create a new user
+// Route::put('/users/{id}', [UserController::class, 'update']); // Update a user by ID
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
