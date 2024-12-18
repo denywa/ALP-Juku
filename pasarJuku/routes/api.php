e36b504d010bcd4cd648be7692caa3a316a987f9
denywa <dwa1503@gmail.com>
2024-12-18 20:44:44 +0800

    .

--- pasarJuku/routes/api.php
+++ pasarJuku/routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\Api\shippingAddressController;
 // get user
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,3 +26,18 @@ Route::middleware(['auth:sanctum', 'role:penambak'])->group(function () {
     Route::post('/business-profile', [App\Http\Controllers\Api\businessProfileController::class, 'store']);   // Create a new profile
     Route::get('/business-profile/user', [App\Http\Controllers\Api\businessProfileController::class, 'show']); // Get profile by token logged in
 });
+
+//user
+Route::get('/users', [UserController::class, 'index']); // Retrieve all users
+Route::get('/users/{id}', [UserController::class, 'show']); // Retrieve a single user by ID
+Route::post('/users', [UserController::class, 'store']); // Create a new user
+// Route::put('/users/{id}', [UserController::class, 'update']); // Update a user by ID
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+//shipping address
+
+Route::get('shipping-addresses', [shippingAddressController::class, 'index']);
+Route::get('shipping-addresses/{id}', [shippingAddressController::class, 'show']);
+Route::post('shipping-addresses', [shippingAddressController::class, 'store']);
+Route::put('shipping-addresses/{id}', [shippingAddressController::class, 'update']);
+Route::delete('shipping-addresses/{id}', [shippingAddressController::class, 'destroy']);
