2d76fc1e1805753eed8cb19bc6e175298e8ae479
denywa <dwa1503@gmail.com>
2025-01-09 09:29:24 +0800

    fix conflict

--- pasarJuku/routes/api.php
+++ pasarJuku/routes/api.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\shippingAddressController;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Api\businessProfileController;
-use App\Http\Controllers\Api\OrderItemController;
-use App\Http\Controllers\Api\PaymentProcessController;
-use App\Http\Controllers\Api\ReviewController;
+
 
 Route::get("/", function (Request $request) {
     return response()->json([
@@ -55,23 +53,3 @@ Route::post('shipping-addresses', [shippingAddressController::class, 'store']);
 Route::put('shipping-addresses/{id}', [shippingAddressController::class, 'update']);
 Route::delete('shipping-addresses/{id}', [shippingAddressController::class, 'destroy']);
 
-//order items
-Route::get('order-items', [OrderItemController::class, 'index']);
-Route::get('order-items/{id}', [OrderItemController::class, 'show']);
-Route::post('order-items', [OrderItemController::class, 'store']);
-Route::put('order-items/{id}', [OrderItemController::class, 'update']);
-Route::delete('order-items/{id}', [OrderItemController::class, 'destroy']);
-
-//payment process
-Route::get('payment-processes', [PaymentProcessController::class, 'index']);
-Route::get('payment-processes/{id}', [PaymentProcessController::class, 'show']);
-Route::post('payment-processes', [PaymentProcessController::class, 'store']);
-Route::put('payment-processes/{id}', [PaymentProcessController::class, 'update']);
-// Route::delete('payment-processes/{id}', [PaymentProcessController::class, 'destroy']);
-
-//review
-Route::get('reviews', [ReviewController::class, 'index']);
-Route::get('reviews/{id}', [ReviewController::class, 'show']);
-Route::post('reviews', [ReviewController::class, 'store']);
-Route::put('reviews/{id}', [ReviewController::class, 'update']);
-// Route::delete('reviews/{id}', [ReviewController::class, 'destroy']);
